37efa8ac0de7bb6b196d4e07348a630ffc4532fc
Jeffrei <hjefrei@gmail.com>
2022-07-08 10:05:03 -0400

    cambios en el index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const xmljs2 = require('xmljs2');
 const axios = require('axios');
+const fs = require('fs')
 
 const getXml = async() => {
     const data = await axios.get('http://www.dneonline.com/calculator.asmx?wsdl');
@@ -8,10 +9,15 @@ const getXml = async() => {
 
 
 const converToJson = (data) =>{
-    const parser = xmljs2.Parser();
 
-    parser.parseString(data.data, (err, result) => {
-        console.dir(result);
+    xmljs2.parseString(data.data, { mergeAttrs: true }, (err, result) => {
+        const json = JSON.stringify(result, null, 4)
+        const cambJson = Object.keys(json);
+        console.log(cambJson)
+        fs.writeFile('converted.json', json, (err)=>{
+            if(err) throw err;
+        })
+        // console.log("JSON saved sucessfully");
     });
 }
 
